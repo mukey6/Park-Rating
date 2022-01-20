88329c59d2a450b4425d90b6187a6f55a4736af2
Rick Johnson <carl4917@umn.edu>
2022-01-19 20:25:20 -0600

    adding login route in homeRoutes.js

--- Routes/homeRoutes.js
+++ Routes/homeRoutes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+
+router.get('/login', (req, res) =>{
+   if (req.session.loggedIn){
+    res.redirect('/');
+    return;
+   }
+    res.render('login');
+  });
+  
+  module.exports = router;
+  
