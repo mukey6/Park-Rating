c7e083b88aaca498b4b1ffc622a5ce842a66e6d9
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-01-24 15:16:51 -0600

    update sign in/out files

--- public/javascript/delete-park.js
+++ public/javascript/delete-park.js
@@ -1,19 +1,21 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
-          // get end point and add it to fetch
-    const deletedPost = await fetch(`/????/${id}`, {
-        method: 'DELETE'
-      });
-  if(deletedPost.ok){
- // redirect to user's page to show their added parks
-    document.location.replace('//')
-  }else {
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  // get end point and add it to fetch
+  const deletedPost = await fetch(`/????/${id}`, {
+    method: "DELETE",
+  });
+  if (deletedPost.ok) {
+    // redirect to user's page to show their added parks
+    document.location.replace("//");
+  } else {
     alert(response.statusText);
   }
-  }
-  // find a class for the delete button
-  document.querySelector('.delete-park-btn???').addEventListener('click', deleteFormHandler);
+}
+// find a class for the delete button
+document
+  .querySelector(".delete-park-btn???")
+  .addEventListener("click", deleteFormHandler);
