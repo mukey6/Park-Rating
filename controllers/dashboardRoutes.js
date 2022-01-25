d92c90c0d275023db1c9be6727f10b6a23f8c6f7
Rick Johnson <carl4917@umn.edu>
2022-01-24 20:32:14 -0600

    editing models in dashboradroutes and homeroutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -9,27 +9,28 @@ router.get('/', Authorize, (req, res) => {
     where: {
         user_id: req.session.user_id
         },
-      attributes: [
-        'id',
-        'park_name',
-        'park_rate',
-        'user_id',
+        attributes: [
+          'id',
+          'park_name',
+          'park_rate',
+          'user_id',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_input', 'user_id','title'],
-          include: {
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'user_id','post_id'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
             model: User,
-            attributes: ['userID']
+            attributes: ['username']
           }
-        },
-        {
-          model: User,
-          attributes: ['userID']
-        }
-      ]
-    })
+        ]
+      })
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('dashboard', { posts, loggedIn: true });
@@ -43,27 +44,28 @@ router.get('/', Authorize, (req, res) => {
   
 router.get('/edit/:id', Authorize, (req, res) => {
     Post.findByPk(req.params.id, {
-        attributes: [
-            'id',
-            'park_name',
-            'park_rate',
-            'user_id',
-            ],
-          include: [
-            {
-              model: Comment,
-              attributes: ['id', 'comment_input', 'user_id','title'],
-              include: {
-                model: User,
-                attributes: ['userID']
-              }
-            },
-            {
-              model: User,
-              attributes: ['userID']
-            }
-          ]
-        })
+      attributes: [
+        'id',
+        'park_name',
+        'park_rate',
+        'user_id',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'user_id','post_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
       .then(dbPostData => {
         if (dbPostData) {
           const post = dbPostData.get({ plain: true });
