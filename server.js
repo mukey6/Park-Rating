441ee65ee01945b956c20e6461c9debf7947847f
hatetavian <tavianjdawson123@gmail.com>
2022-01-24 19:15:45 -0600

    tweak of server file

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
