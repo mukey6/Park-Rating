0d4849f309a36b86358b6764a3cc9af2931f6b75
Rick Johnson <carl4917@umn.edu>
2022-01-24 19:19:49 -0600

    entering template code for config-> connection.js

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+
+//const sequelize = 
+//  ? new Sequelize()
+//  : new Sequelize(, {
+//      host: '',
+//      dialect: 'mysql',
+//      port: 
+//    });
+
+
+module.exports = sequelize;
