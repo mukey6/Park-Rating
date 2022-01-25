af24c36be87147cd427f91af003a1762d419f7af
Rick Johnson <carl4917@umn.edu>
2022-01-24 20:12:07 -0600

    adding get all user and delete profile/user routes

--- controllers/apiroutes/userRoutes.js
+++ controllers/apiroutes/userRoutes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const Authorize = require('../../utils/authorize');
 
+
+ // route to get all lsi of all existing users 
+
+ router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
 //create user/profile
 router.post('/', (req, res) => {
     User.create({
@@ -59,6 +74,26 @@ router.post('/logout', (req, res) => {
       res.status(404).end();
     }
   });
+
+  
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 
   module.exports = router;
